7c3437b433ef1f4c9fa8eee482e0557b61b96556
Icaro Ramos <polosnramos@hotmail.com>
2023-05-05 14:47:43 -0300

    feat: fontsLoaded

--- App.tsx
+++ App.tsx
@@ -1,5 +1,19 @@
+import { ThemeProvider } from "styled-components/native";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import theme from "./src/theme";
 import { Groups } from "@screens/Groups";
+import { Loading } from "@components/Loading";
 
 export default function App() {
-  return <Groups />;
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  return (
+    <ThemeProvider theme={theme}>
+      {fontsLoaded ? <Groups /> : <Loading />}
+    </ThemeProvider>
+  );
 }
