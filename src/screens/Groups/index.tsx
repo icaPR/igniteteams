fe88663bfc862ae48e8736274af4dddb9d088d10
Icaro Ramos <polosnramos@hotmail.com>
2023-05-18 04:06:25 -0300

    feat: use components Header, Hightlight, Button

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,18 +1,29 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Header } from "@components/Header";
+import { Container } from "./styles";
+import { Hightlight } from "@components/Hightlight";
+import { GroupCard } from "@components/GroupCard";
+import { ListEmpty } from "@components/ListEmpty";
+import { useState } from "react";
+import { FlatList } from "react-native";
+import { Button } from "@components/Button";
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([]);
+
   return (
-    <View style={styles.container}>
-      <Text>Groups</Text>
-    </View>
+    <Container>
+      <Header />
+      <Hightlight title="Turmas" subtitle="jogue com a sua turma" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
+      />
+      <Button title="Criar nova turma" />
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
