cb3117024a69f99074a8a6f7eb6789bf906cbef7
Icaro Ramos <polosnramos@hotmail.com>
2023-05-21 18:58:20 -0300

    feat: use Routes

--- App.tsx
+++ App.tsx
@@ -5,9 +5,9 @@ import {
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
 import theme from "./src/theme";
-import { Groups } from "@screens/Groups";
 import { Loading } from "@components/Loading";
 import { StatusBar } from "react-native";
+import { Routes } from "./src/routes";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -19,7 +19,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Groups /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </ThemeProvider>
   );
 }
