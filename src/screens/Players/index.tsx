35585809625483c21c95dd44be0c02a3df27dd82
Icaro Ramos <polosnramos@hotmail.com>
2023-05-22 04:00:21 -0300

    feat: useRoute in "Players" screen

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -9,18 +9,22 @@ import { useState } from "react";
 import { PlayerCard } from "@components/PlayerCard";
 import { ListEmpty } from "@components/ListEmpty";
 import { Button } from "@components/Button";
+import { useRoute } from "@react-navigation/native";
+
+type RouteParams = {
+  group: string;
+};
 
 export function Players() {
   const [team, setTeam] = useState("time a");
   const [players, setPlayers] = useState(["time a", "time b"]);
+  const route = useRoute();
+  const { group } = route.params as RouteParams;
 
   return (
     <Container>
       <Header showBackButton />
-      <Hightlight
-        title="Nome da Turma"
-        subtitle="adcione a galera e separe os times"
-      />
+      <Hightlight title={group} subtitle="adcione a galera e separe os times" />
       <Form>
         <Input placeholder="Nome da pessoa" autoCorrect={false} />
         <ButtonIcon icon="add" />
